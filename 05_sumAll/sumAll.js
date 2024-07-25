d80daff6dc73b66f7d6875343bdf57425bb7d60b
Souhayl-s <souhaylsejjari1@gmail.com>
2024-07-25 13:11:04 -0400

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (lowerLimit, upperLimit) {
+  let isInteger = Number.isInteger(lowerLimit) && Number.isInteger(upperLimit)
+  let isPositive = lowerLimit >= 0 && upperLimit >= 0;
+  if ( isInteger && isPositive) {
+    if (lowerLimit > upperLimit) {
+      [lowerLimit, upperLimit] = [upperLimit, lowerLimit]; // swap values
+    }
+    let sum = 0;
+    for (let num = lowerLimit; num <= upperLimit; ++num) {
+      sum += num;
+    }
+    return sum;
+  }
+  return "ERROR";
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
